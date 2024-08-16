a230c10ea6bf2732da8cb86b4345f7eed6635611
felipejimcam25 <andreyfelipejimenezcampos@gmail.com>
2024-08-15 19:08:57 -0600

    Funcionalidades nuevas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-const nav = document.querySelector("#nav");
+const navMenu = document.querySelector("#nav");
 const open = document.querySelector("#open");
 const close = document.querySelector("#close");
 
 open.addEventListener("click", () => {
-    nav.classList.add("visible");
+    navMenu.classList.add("visible");
 })
 
 close.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
+    navMenu.classList.remove("visible");
+});
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav-link');
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav');
+    //Cuando se da click en cada nav__link, se remueve show-menu
+    navMenu.classList.remove('visible')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction)); /*for each:Es un ciclo, n:recore cada uno de los valores*/
